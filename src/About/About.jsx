49cc6d9d04833dafc7bb3112bb2bb797c757d30d
Lexlinkan <alexander-lindqvist@live.se>
2024-03-15 15:16:02 +0100

    about should be responsive

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,46 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { body, logo, nav, navItem, spanContainer, title, subtitle, policyTitle, cardContainer, card, cardTitle, cardSubtitle, linkStyle } from "./styles";
+import './about.css';
+import Navbar from "../Navbar/Navbar";
 
-import logoHome from "../assets/CCS_COLOR.png";
+// Bilder
 import omOss from "../assets/omOss.jpeg";
 
 const About = () => {
   return (
-    <div style={body}>
-      <Link to="/">
-        <img style={logo} src={logoHome} alt="Logga" />
-      </Link>
-      <div style={nav}>
-        <Link style={linkStyle} to="/tjanster">
-          <span style={navItem}>VÅRA TJÄNSTER</span>
-        </Link>
-        <Link style={linkStyle} to="/markservice">
-          <span style={navItem}>MARKSERVICE</span>
-        </Link>
-        <Link style={linkStyle} to="/referenser">
-          <span style={navItem}>REFERENSER</span>
-        </Link>
-        <Link style={linkStyle} to="/kvalitetmiljo">
-          <span style={navItem}>KVALITET & MILJÖ</span>
-        </Link>
-        <Link style={linkStyle} to="/offert">
-          <span style={navItem}>BEGÄR OFFERT</span>
-        </Link>{" "}
-        <Link style={linkStyle} to="/ledigajobb">
-          <span style={navItem}>LEDIGA JOBB</span>
-        </Link>
-        <Link style={linkStyle} to="/omoss">
-          <span style={navItem}>OM OSS</span>
-        </Link>
-        <Link style={linkStyle} to="/kontakt">
-          <span style={navItem}>KONTAKT</span>
-        </Link>
-      </div>
+    <div className="bodyAbout">
+      <Navbar />
       <img src={omOss} style={{ width: "75%" }} alt="Våra Tjänster image" />
-      <div style={spanContainer}>
-        <span style={title}>Ett städbolag att lita på</span>
-        <span style={subtitle}>
+      <div className="spanContainerAbout">
+        <span className="titleAbout">Ett städbolag att lita på</span>
+        <span className="subtitleAbout">
           CC Service är ett mindre städbolag med det stora företagets kompetens
           och erfarenhet. Vi erbjuder olika städtjänster hos privata företag
           samt hushållsnära tjänster till privatpersoner. Vi är certifierade
@@ -48,35 +20,35 @@ const About = () => {
           vara en attraktiv arbetsgivare och ett företag med god etik.
         </span>
       </div>
-      <div style={cardContainer}>
-        <div style={card}>
-          <span style={cardTitle}>
+      <div className="cardContainerAbout">
+        <div className="cardAbout">
+          <span className="cardTitleAbout">
             OM OSS
-            <p style={cardSubtitle}>
+            <p className="cardSubtitleAbout">
               CC Service startades 2014 men har en gedigen och lång erfarenhet.
               Våra kunder finns i Storstockholm inom såväl den privata som den
               offentliga sektorn. Vi strävar efter att erbjuda hushållsnära
               tjänster med hög kvalitet.
             </p>
-            <p style={cardSubtitle}>
+            <p className="cardSubtitleAbout">
               Vår affärsidé är att utföra miljöanpassad lokalvård och andra
               servicetjänster med kvalitet och bra kundservice. Vår
               konkurrenskraft fokuseras på vårt kunnande, vårt engagemang, våra
               resurser och våra priser.
             </p>
-            <p style={cardSubtitle}>
+            <p className="cardSubtitleAbout">
               Framförallt arbetar vi för att skapa en ökad kundnöjdhet, men även
               för att hitta en hållbar utveckling för företaget. Vi vill ha
               motiverade medarbetare som trivs med att jobba hos oss.
             </p>
           </span>
         </div>
-        <div style={card}>
-          <span style={cardSubtitle}>
-            <span style={cardTitle}>VÅRA POLICYS I SAMMANDRAG</span>
+        <div className="cardAbout1">
+          <span className="cardSubtitleAbout">
+            <span className="cardTitleAbout1">VÅRA POLICYS</span>
             <ul>
               <li>
-                <p style={policyTitle}>
+                <p className="policyTitleAbout">
                   KVALITETSPOLICY
                   {
                     "- att uppfylla kunders krav/behov i rätt tid, på rätt sätt och med hög och jämn kvalitet."
@@ -84,7 +56,7 @@ const About = () => {
                 </p>
               </li>
               <li>
-                <p style={policyTitle}>
+                <p className="policyTitleAbout">
                   MILJÖPOLICY
                   {
                     "- minska miljöpåverkan genom mindre förorening, avfall och användning av naturresurser."
@@ -92,7 +64,7 @@ const About = () => {
                 </p>
               </li>
               <li>
-                <p style={policyTitle}>
+                <p className="policyTitleAbout">
                   PERSONALPOLICY
                   {
                     "- skapa kompetenta/engagerade medarbetare, som kan bidra till företagets utveckling. "
@@ -100,7 +72,7 @@ const About = () => {
                 </p>
               </li>
               <li>
-                <p style={policyTitle}>
+                <p className="policyTitleAbout">
                   ARBETSMILJÖPOLICY
                   {
                     "– skall leda till ett gott arbetsklimat, bra hälsa, låg sjukfrånvaro och få arbetsskador."
@@ -108,7 +80,7 @@ const About = () => {
                 </p>
               </li>
               <li>
-                <p style={policyTitle}>
+                <p className="policyTitleAbout">
                   JÄMSTÄLLDHETSPOLICY
                   {
                     "– ett målinriktat jämställdhetsarbete med lika möjlighet till anställning och befordran."
@@ -116,7 +88,7 @@ const About = () => {
                 </p>
               </li>
               <li>
-                <p style={policyTitle}>
+                <p className="policyTitleAbout">
                   DROGPOLICY
                   {
                     "– alkohol/droger ska inte användas. Vi förebygger och motverkar missbruksproblem."
@@ -124,7 +96,7 @@ const About = () => {
                 </p>
               </li>
               <li>
-                <p style={policyTitle}>
+                <p className="policyTitleAbout">
                   ETIKPOLICY
                   {
                     "– vi ska präglas av god etik, bry oss om vår omvärld, respektera mänskliga rättigheter och stå för löften. KRÄNKANDE SÄRBEHANDLING OCH SEXUELLA TRAKASSERIER – kränkande särbehandling, mobbning eller sexuella trakasserier tolereras ej på våra arbetsplatser."
